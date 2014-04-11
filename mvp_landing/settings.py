2539de7817e53d51faf2f90bcc26ac204f0a24f1
Andy Giffen <giffen@gmail.com>
2014-04-11 09:45:24 -0700

    collect Static pages and build model form

--- mvp_landing/settings.py
+++ mvp_landing/settings.py
@@ -83,7 +83,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 # Template location
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR),"static","templates"),
 )
+
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","media")
+    STATIC_FILES_DIRS = (
+        os.path.join(os.path.dirname(BASE_DIR),"static","static")
+    )
--- mvp_landing/urls.py
+++ mvp_landing/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import patterns, include, url
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -8,3 +12,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
