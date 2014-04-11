3c3cff537397680b738731da52e1342c17ed7b92
Andy Giffen <giffen@gmail.com>
2014-04-11 10:51:10 -0700

    Add Bootstrap

--- mvp_landing/settings.py
+++ mvp_landing/settings.py
@@ -93,6 +93,6 @@ if DEBUG:
     MEDIA_URL = '/media/'
     STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","media")
-    STATIC_FILES_DIRS = (
-        os.path.join(os.path.dirname(BASE_DIR),"static","static")
+    STATICFILES_DIRS = (
+        os.path.join(os.path.dirname(BASE_DIR),"static","static"),
     )
